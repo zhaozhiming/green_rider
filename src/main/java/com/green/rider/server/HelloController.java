d8c3def06129619cafbf8e57e8c68fcdfb723420
zhaozhiming <kingzzm1982@sina.com>
2013-07-20 13:20:13 +0800

    add create user api

--- src/main/java/com/green/rider/server/HelloController.java
+++ src/main/java/com/green/rider/server/HelloController.java
@@ -1,16 +1,33 @@
 package com.green.rider.server;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    @RequestMapping(value = "/api/user/create", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String createUser(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        JSONObject user = new JSONObject();
+        return user.toString();
+    }
+
+    @RequestMapping(value = "/api/user/create", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String printWelcome(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        JSONObject user = new JSONObject();
+        return user.toString();
+    }
+
 }
