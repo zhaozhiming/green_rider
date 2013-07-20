cc7c140cdb85947864cbb0d95eb745ff5e9445d1
zhaozhiming <kingzzm1982@sina.com>
2013-07-20 13:36:33 +0800

    rename controller

--- src/main/java/com/green/rider/server/GreenRiderController.java
+++ src/main/java/com/green/rider/server/GreenRiderController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-public class HelloController {
+public class GreenRiderController {
     @RequestMapping(value = "/api/user/create", method = RequestMethod.POST)
     public
     @ResponseBody
